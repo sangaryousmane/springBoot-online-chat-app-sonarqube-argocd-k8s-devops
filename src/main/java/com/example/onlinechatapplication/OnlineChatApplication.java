15b5d184ebc32194ee2ed413b99f5f24d72167d7
ousmane <sangaryousmane@gmail.com>
2023-07-06 06:55:31 +0800

    Add socket config

---
+++ src/main/java/com/example/onlinechatapplication/OnlineChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinechatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChatApplication.class, args);
+    }
+
+}
